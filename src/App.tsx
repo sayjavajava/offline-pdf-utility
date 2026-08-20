1fbf0b4089f3e23f0821e0aa54e1fa154bff8e13
Mayank Aggarwal <19813146+sayjavajava@users.noreply.github.com>
2026-08-20 10:42:33 +0530

    Repo hygiene: real favicon, drop two unused scaffold leftovers (#36)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ const App = () => (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
